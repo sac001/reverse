965ecafd792876baa4f5813c8a5a2cf71fc0500d
Stefan Claas <sacenator@gmail.com>
2021-12-24 18:26:35 +0100

    Add files via upload

---
+++ reverse.go
@@ -0,0 +1,24 @@
+﻿package main
+
+import (
+    "io/ioutil"
+    "fmt"
+    "os"
+)
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+func main() {
+	input, _ := ioutil.ReadAll(os.Stdin)
+	if len(input) == 0 {
+		return
+	}
+	
+	fmt.Print(Reverse(string(input)))
+}
